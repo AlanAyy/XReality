cd1d70ea17466b5f05cc832eeda77ab046fb0326
Alan A. <58347931+AlanAyy@users.noreply.github.com>
2025-03-04 00:31:09 -0700

    Created UDPListener Script

---
+++ Assets/Scripts/UDPListener.cs
@@ -0,0 +1,68 @@
+// Source: https://gist.github.com/unitycoder/7ad714e72f5fed1c50c6d7a188082388#file-udplistener-cs-L55
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class UDPListener : MonoBehaviour
+{
+    UdpClient clientData;
+    int portData = 23232;
+    public int receiveBufferSize = 120000;
+
+    public bool showDebug = true;
+    IPEndPoint ipEndPointData;
+    private object obj = null;
+    private System.AsyncCallback AC;
+    byte[] receivedBytes;
+
+    void Start()
+    {
+        InitializeUDPListener();
+    }
+    public void InitializeUDPListener()
+    {
+        ipEndPointData = new IPEndPoint(IPAddress.Any, portData);
+        clientData = new UdpClient();
+        clientData.Client.ReceiveBufferSize = receiveBufferSize;
+        clientData.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, optionValue: true);
+        clientData.ExclusiveAddressUse = false;
+        clientData.EnableBroadcast = true;
+        clientData.Client.Bind(ipEndPointData);
+        clientData.DontFragment = true;
+        if (showDebug) Debug.Log("BufSize: " + clientData.Client.ReceiveBufferSize);
+        AC = new System.AsyncCallback(ReceivedUDPPacket);
+        clientData.BeginReceive(AC, obj);
+        Debug.Log("UDP - Start Receiving..");
+    }
+
+    void ReceivedUDPPacket(System.IAsyncResult result)
+    {
+        //stopwatch.Start();
+        receivedBytes = clientData.EndReceive(result, ref ipEndPointData);
+
+        ParsePacket();
+
+        clientData.BeginReceive(AC, obj);
+
+        //stopwatch.Stop();
+        //Debug.Log(stopwatch.ElapsedTicks);
+        //stopwatch.Reset();
+    } // ReceiveCallBack
+
+    void ParsePacket()
+    {
+        // work with receivedBytes
+        Debug.Log("receivedBytes len = " + receivedBytes.Length);
+    }
+
+    void OnDestroy()
+    {
+        if (clientData != null)
+        {
+            clientData.Close();
+        }
+    }
+}
