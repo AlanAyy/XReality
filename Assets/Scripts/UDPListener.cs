011b02f5a568fd471c4b97bd5d5b08bce05e8d1f
Alan A. <58347931+AlanAyy@users.noreply.github.com>
2025-03-10 17:52:53 -0600

    Added Camera Feed and More Buttons
    Required adding some multithreading on the RPi side but it works!

--- Assets/Scripts/UDPListener.cs
+++ Assets/Scripts/UDPListener.cs
@@ -1,4 +1,4 @@
-// Source: https://gist.github.com/unitycoder/7ad714e72f5fed1c50c6d7a188082388#file-udplistener-cs-L55
+// Based on this code: https://gist.github.com/unitycoder/7ad714e72f5fed1c50c6d7a188082388#file-udplistener-cs-L55
 
 using System.Collections;
 using System.Collections.Generic;
@@ -6,12 +6,13 @@ using System.Net;
 using System.Net.Sockets;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UDPListener : MonoBehaviour
 {
     UdpClient clientData;
-    int portData = 23232;
-    public int receiveBufferSize = 120000;
+    int defaultPort = 23232;
+    public int receiveBufferSize = 65535;
 
     public bool showDebug = true;
     IPEndPoint ipEndPointData;
@@ -20,26 +21,35 @@ public class UDPListener : MonoBehaviour
     byte[] receivedBytes;
 
     private Texture2D receivedTexture;
-    public UnityEngine.UI.RawImage display;  // Assign this in Unity Inspector
+    public RawImage display;  // Assign this in Unity Inspector
     private bool newFrameAvailable = false;
+    private bool resetFrame = false;
     private object frameLock = new object();
+
+    private const string DEFAULT_IP = "255.255.255.255";
+    private string crawlerIP = DEFAULT_IP;
+    private string localIP = null;
+    // private const int TIMEOUT_SECS = 5;  // seconds
+    private System.DateTime lastConnectionTime;
 
     void Start()
     {
         InitializeUDPListener();
-        receivedTexture = new Texture2D(640, 480);  // Match Pi Camera resolution   
+        receivedTexture = new Texture2D(640, 480);  // Match Pi Camera resolution
+        localIP = GetLocalIPAddress();
+        if (showDebug) Debug.Log("Local IP: " + localIP);
     }
 
     public void InitializeUDPListener()
     {
-        ipEndPointData = new IPEndPoint(IPAddress.Any, portData);
-        clientData = new UdpClient();
+        InitializeUDPListener(IPAddress.Any);
+    }
+
+    public void InitializeUDPListener(IPAddress address)
+    {
+        ipEndPointData = new IPEndPoint(address, defaultPort);
+        clientData = new UdpClient(ipEndPointData);
         clientData.Client.ReceiveBufferSize = receiveBufferSize;
-        clientData.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, optionValue: true);
-        clientData.ExclusiveAddressUse = false;
-        clientData.EnableBroadcast = true;
-        clientData.Client.Bind(ipEndPointData);
-        clientData.DontFragment = true;
         if (showDebug) Debug.Log("BufSize: " + clientData.Client.ReceiveBufferSize);
         AC = new System.AsyncCallback(ReceivedUDPPacket);
         clientData.BeginReceive(AC, obj);
@@ -49,9 +59,23 @@ public class UDPListener : MonoBehaviour
     void ReceivedUDPPacket(System.IAsyncResult result)
     {
         receivedBytes = clientData.EndReceive(result, ref ipEndPointData);
+        Debug.Log("Received UDP packet from " + ipEndPointData.Address + ":" + ipEndPointData.Port);
+        // if (ipEndPointData.Address.ToString() == localIP) return;  // Ignore local packets
         ParsePacket();
         clientData.BeginReceive(AC, obj);
     } // ReceiveCallBack
+
+    public static string GetLocalIPAddress()
+    {
+        // Source: https://stackoverflow.com/questions/6803073/get-local-ip-address
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) return ip.ToString();
+        }
+        Debug.LogError("No network adapters with an IPv4 address in the system!");
+        return null;
+    }
 
     [Button]
     public void SendTestUDPPacket()
@@ -66,8 +90,13 @@ public class UDPListener : MonoBehaviour
         try
         {
             byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
-            clientData.Send(data, data.Length, new IPEndPoint(IPAddress.Parse("255.255.255.255"), portData));
-            if (showDebug) Debug.Log($"Sent UDP Packet to {ipEndPointData.Address}:{ipEndPointData.Port}");
+            // TODO: Could optimize here?
+            IPEndPoint crawlerEndPoint = new IPEndPoint(IPAddress.Parse(crawlerIP), defaultPort);
+            clientData.Send(data, data.Length, crawlerEndPoint);
+            if (showDebug) Debug.Log($"Sent UDP Packet to {crawlerEndPoint.Address}:{crawlerEndPoint.Port}");
+            // Handle disconnection locally
+            if (message == "disconnect") HandleDisconnected();
+            // All other commands should be handled by the PiCrawler
         }
         catch (System.Exception e)
         {
@@ -75,36 +104,83 @@ public class UDPListener : MonoBehaviour
         }
     }
 
+    void HandleConnected()
+    {
+        if (showDebug) Debug.Log("Connected to RealityCrawler!");
+        if (crawlerIP != DEFAULT_IP) return;  // Ignore if already connected
+        lastConnectionTime = System.DateTime.Now;
+        string newIP = ipEndPointData.Address.ToString();
+        if (newIP != DEFAULT_IP && newIP != localIP) {
+            crawlerIP = newIP;
+            if (showDebug) Debug.Log($"Crawler IP set to {crawlerIP}");
+            lastConnectionTime = System.DateTime.Now;
+        }
+    }
+
+    void HandleDisconnected()
+    {
+        if (showDebug) Debug.Log("Disconnected from RealityCrawler");
+        if (crawlerIP == DEFAULT_IP) return;  // Ignore if already disconnected
+        crawlerIP = DEFAULT_IP;
+        if (showDebug) Debug.Log($"Crawler IP reset to {crawlerIP}");
+        resetFrame = true;
+    }
+
     void ParsePacket()
     {
-        // work with receivedBytes
-        if (showDebug) Debug.Log("receivedBytes len = " + receivedBytes.Length);
-        if (receivedBytes.Length < 100)
-        {
-            if (showDebug) Debug.Log("Message: " + System.Text.Encoding.Default.GetString(receivedBytes));
+        // TODO: Not really thread safe... Maybe pass the receivedBytes and IP as arguments
+        if (showDebug) Debug.Log("receivedBytes length: " + receivedBytes.Length);
+        // Handle small messages (commands)
+        if (receivedBytes.Length < 1000) {
+            string message = System.Text.Encoding.Default.GetString(receivedBytes);
+            // Ignore local or foreign packets
+            if (ipEndPointData.Address.ToString() == localIP) return;
+            else if (crawlerIP != DEFAULT_IP && ipEndPointData.Address.ToString() != crawlerIP) {
+                if (showDebug) Debug.Log($"Ignoring packet from unknown IP: {ipEndPointData.Address}");
+                return;
+            }
+            // Handle commands
+            if (message == "connected") HandleConnected();
+            else if (message == "disconnected") HandleDisconnected();
+            if (showDebug) Debug.Log($"Message: {message}");
         }
-        else
-        {
-            if (showDebug) Debug.Log("Image received");
+        // Handle big messages (image frames)
+        else {
             lock (frameLock)  // Ensure thread safety
             {
+                // if (showDebug) Debug.Log("Image received");
                 newFrameAvailable = true;
             }
         }
-        
     }
 
     void Update()
     {
+        // Check if connection is stale (DISABLED FOR NOW)
+        // if (crawlerIP != DEFAULT_IP && System.DateTime.Compare(lastConnectionTime.AddSeconds(TIMEOUT_SECS), System.DateTime.Now) < 0) SendUDPPacket("disconnect");
+        // Update the display with the new frame
         if (newFrameAvailable)
         {
             lock (frameLock)  // Avoid thread conflicts
             {
                 receivedTexture.LoadImage(receivedBytes);  // Decode JPEG
                 newFrameAvailable = false;
-                if (showDebug) Debug.Log("Frame loaded");
+                // if (showDebug) Debug.Log("Frame loaded");
+                display.texture = receivedTexture;  // Apply texture to UI
             }
+        }
+        if (resetFrame)
+        {
+            receivedTexture = new Texture2D(640, 480);
+            Color32[] whitePixels = new Color32[640 * 480];
+            for (int i = 0; i < whitePixels.Length; i++)
+            {
+                whitePixels[i] = new Color32(255, 255, 255, 255);  // White color
+            }
+            receivedTexture.SetPixels32(whitePixels);
+            receivedTexture.Apply();
             display.texture = receivedTexture;  // Apply texture to UI
+            resetFrame = false;
         }
     }
 
